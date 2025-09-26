8aa46396a1142d3743d711aa92c06dc877d82ccc
grufre349 <grufre349@gmail.com>
2025-09-26 18:07:17 +0200

    added pokedex page and got stuck ow because of default Import path not working. savepoint before trying to fix it

---
+++ src/app/pokedex/page.js
@@ -0,0 +1,9 @@
+const Pokedex = () => {
+	return (
+		<section>
+			<h1>Pokedex</h1>
+		</section>
+	);
+};
+
+export default Pokedex;
---
+++ src/components/PokemonCard.js
@@ -0,0 +1,10 @@
+// actual PokemonCard with Detailed Info about each
+const PokemonCard = ({ pokemon }) => {
+	return (
+		<section>
+			<h3>{pokemon}</h3>
+		</section>
+	);
+};
+
+export default PokemonCard;
